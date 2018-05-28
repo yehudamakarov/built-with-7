990c80811d92d250978ee8b6025eff93379725f6
github username <github email address>
2018-05-27 21:37:46 -0400

    added routes for callback login and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # login_stuff
+  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
+  match '/logout' => 'sessions#destroy', via: [:get, :post]
+
+  # home stuff
   root 'application#welcome'
+
+
 end
