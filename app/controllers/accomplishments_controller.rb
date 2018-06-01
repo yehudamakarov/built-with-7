25a94b30404d886bb2c12983618a92f7308efb65
github username <github email address>
2018-06-01 18:20:32 -0400

    Making an accomplishment! must figure out how to make a task with a different datetime of "today" but only using the day of the week.

--- app/controllers/accomplishments_controller.rb
+++ app/controllers/accomplishments_controller.rb
@@ -7,15 +7,23 @@ class AccomplishmentsController < ApplicationController
   end
 
   def new
-    @accomplishment = Accomplishment.new
     @user = current_user
+    @accomplishment = Accomplishment.new(user: @user)
+
   end
 
   def create
+    @accomplishment = Accomplishment.new(accomplishment_params)
     binding.pry
   end
 
   def show
     #code
+  end
+
+  private
+
+  def accomplishment_params
+    params.require(:accomplishment).permit(:title, :effect, :date_time, day_ids: [])
   end
 end
