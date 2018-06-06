ccf9fa2d3ea2864836e101d78325bf52736b7297
github username <github email address>
2018-06-05 20:16:45 -0400

    adding error messages.

--- app/controllers/accomplishments_controller.rb
+++ app/controllers/accomplishments_controller.rb
@@ -24,11 +24,11 @@ class AccomplishmentsController < ApplicationController
   def create
 
     # Sets the date_time of the accomplishment to the last occuring weekday specified on the form, using the time of day specified by the form.
-
+    @user = current_user
     @accomplishment = Accomplishment.new(accomplishment_params) do |a|
       current_datetime = a.date_time_of_task_with_current_date
       a.date_time = a.date_find(current_datetime, accomplishment_params[:day])
-      a.user = current_user
+      a.user = @user
     end
 
     if @accomplishment.save
