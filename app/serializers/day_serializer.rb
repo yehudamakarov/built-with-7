81b2e8e233579053dab3f6bc1ee60cc25ff4a28c
github username <github email address>
2018-06-21 04:57:35 -0400

    finally have control over nested attributes through active_model_serializers

--- app/serializers/day_serializer.rb
+++ app/serializers/day_serializer.rb
@@ -2,6 +2,12 @@ class DaySerializer < ActiveModel::Serializer
   attributes :id, :accomplishments
 
   def accomplishments
-    object.accomplishments
+    object.accomplishments.map do |acc|
+      {
+        title: acc.title,
+        id: acc.id,
+        human_time: acc.human_time
+      }
+    end
   end
 end
