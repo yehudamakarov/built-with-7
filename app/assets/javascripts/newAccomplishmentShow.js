2f338d8a5a811fcd42a331c9bb622354a88d426d
github username <github email address>
2018-06-23 23:02:31 -0400

    removed some commets, updated the checklist. edited the error's show class.

--- app/assets/javascripts/newAccomplishmentShow.js
+++ app/assets/javascripts/newAccomplishmentShow.js
@@ -4,18 +4,16 @@ $(() => {
         const values = $(e.target).serialize();
         const postResp = $.post('/accomplishments', values);
         postResp.done((respData) => {
-            // so this is always going to run and the IF/ELSE in the controller
-            // is deciding what to send back to this request.
-
-            // We unbind the eventListener from `this` which is the element that
-            // the function was bound to.
             const toInsert = new TemplateEntry(respData);
             const newHTML = toInsert.templateString();
             $('#js-accomplishment-info')
                 .html(newHTML)
                 .addClass('open-up');
         }).error((respData) => {
-            $('h1').html(respData.responseJSON.title['0']);
+            let errorMessage = 'There was an error: Title ';
+            errorMessage += respData.responseJSON.title['0'];
+            $('#js-accomplishment-info').html(errorMessage)
+                .addClass('open-up-error');
         });
     });
 
