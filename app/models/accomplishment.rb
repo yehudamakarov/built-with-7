4de4e57905fc076bf1c40fcc53bad7875e3a4dca
github username <github email address>
2018-08-20 21:06:47 -0400

    fixed update accomplishment and added redirect link on new accomplishment

--- app/models/accomplishment.rb
+++ app/models/accomplishment.rb
@@ -21,12 +21,12 @@ class Accomplishment < ApplicationRecord
 
   def update_from_params(params)
     self.assign_attributes(params)
-    day_of_week = self.user.days.find_by(name: self.date_time.strftime('$A'))
+    day_of_week = self.user.days.find_by(name: self.date_time.strftime("%A"))
+    if self.days.empty?
+        self.days << day_of_week
+    end
     self.day = day_of_week.name
     self.human_time = self.time_of_accompmlishment
-    if self.days.empty?
-      self.days << day_of_week
-    end
   end
 
   def date_time_of_task_with_current_date
